950e4773d773952028a1c7451a4060d2526e14d1
Emil Tin <emil@tin.dk>
2025-08-08 12:03:11 +0200

    remove unused js

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,9 +20,6 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
-import { setupSignalDragHandlers } from "./hooks/signal_drag"
-import { setupInputHandlers } from "./hooks/input_handlers"
-import { setupPromptHandlers } from "./hooks/prompt_handlers"
 
 // Define hooks for LiveView
 let Hooks = {}
@@ -110,6 +107,9 @@ let liveSocket = new LiveSocket("/live", Socket, {
 // Topbar + connect
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", _ => topbar.show(300))
+window.addEventListener("phx:page-loading-stop", _ => topbar.hide())
+liveSocket.connect()
+window.liveSocket = liveSocket
 window.addEventListener("phx:page-loading-stop", _ => topbar.hide())
 liveSocket.connect()
 window.liveSocket = liveSocket
